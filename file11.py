aee65166a6e97e07e213530d0c46ba0c4bed3916
bhavishya-8 <109825091+bhavishya-8@users.noreply.github.com>
2023-01-09 22:47:33 +0530

    Update file11.py

--- file11.py
+++ file11.py
@@ -17,17 +17,25 @@ window.geometry('%dx%d+0+0' % (width,height))
 window.configure(background='light steel blue')
 
 
+#Global varibale
+date_output_file11 = 0
+
+
 
 # folder_path = StringVar()
 
 message = tk.Label(window,  text = "Linux controller", bg="slate blue", fg="black", height=2, width=int(window.winfo_screenwidth()/2), font=('times', 30, 'italic bold ')).pack()
 
+
+
+terminal_result = tk.Label(window, text="Terminal Result: ", fg="black", bg="light steel blue", font=('Helvetica 15 underline', 18), )
+terminal_result.place(x=650, y=130)
+dataFrameRight = Frame(window, bd=10, padx=20, relief=RIDGE)
+dataFrameRight.place(x=650, y=170, width=590, height=400)
 #frames left and right
 # terminal_result = tk.Label(window, text="Terminal Result: ", fg="black", bg="light steel blue", font=('Helvetica 15 underline', 18), )
 # terminal_result.place(x=650, y=130)
 
-# dataFrameRight = Frame(window, bd=10, padx=20, relief=RIDGE)
-# dataFrameRight.place(x=650, y=170, width=590, height=400)
 
 # termf = Frame(window)
 # dataFrameRight.pack(side=BOTTOM, fill=X)
@@ -53,7 +61,7 @@ this_2 = tk.Label(window, text="2 --> Calender", fg="black", bg="light steel blu
 this_2.place(x=50, y=140)
 this_2.pack(padx=50 ,pady=5 ,anchor="w")
 
-this_3 = tk.Label(window, text="3 --> SSH", fg="black", bg="light steel blue", font=('Helvetica 15 underline', 18), )
+this_3 = tk.Label(window, text="3 --> SSH(please enter the IP address and \n the command in the terminal)", fg="black", bg="light steel blue", font=('Helvetica 15 underline', 18), )
 this_3.place(x=50, y=160)
 this_3.pack(padx=50 ,pady=5 ,anchor="w")
 
@@ -61,7 +69,7 @@ this_4 = tk.Label(window, text="4 --> Configure httpd server", fg="black", bg="l
 this_4.place(x=50, y=180)
 this_4.pack(padx=50 ,pady=5 ,anchor="w")
 
-this_5 = tk.Label(window, text="5 --> Exit", fg="black", bg="light steel blue", font=('Helvetica 15 underline', 18), )
+this_5 = tk.Label(window, text="5 --> Clear", fg="black", bg="light steel blue", font=('Helvetica 15 underline', 18), )
 this_5.place(x=50, y=200)
 this_5.pack(padx=50 ,pady=5 ,anchor="w")
 
@@ -75,17 +83,29 @@ this_5.pack(padx=50 ,pady=5 ,anchor="w")
 #    ip_varible.place(x=150, y=480)
 
 # URL Message
-
 def prt():
 #    print(url.get())
-   file1.opt(url.get())
+   global date_output_file11
+   # date_output_file11 = file1.opt(url.get())
+   # print ("HELLO")
+   # file1.opt(url.get())
+   date_output_file11 = file1.opt(url.get())
+   # date_output_file11 = "Hello"
+   
+   result = tk.Label(window, text=date_output_file11, width=30, height=10, fg="black", font=('Helvetica 15 underline', 18))
+   result.place(x=690, y=200)
 
 
-lbl_url = tk.Label(window, text="Enter choice:", width=10, height=2, fg="black", bg="light steel blue", font=('times', 15, ' bold '))
-lbl_url.place(x=20, y=440)
+# date_output_file11=5
+# lbl_url = tk.Label(window, text=x, width=10, height=2, fg="black", bg="light steel blue", font=('times', 15, ' bold '))
+
+
+
+lbl_url = tk.Label(window, text="Enter value:", width=10, height=2, fg="black", bg="light steel blue", font=('times', 15, ' bold '))
+lbl_url.place(x=20, y=470)
 
 url = tk.Entry(window, width=40, bg="linen", fg="gray9",font=('times', 15, ' bold '))
-url.place(x=150, y=450)
+url.place(x=150, y=480)
 
 
 # # Path Message
@@ -108,7 +128,7 @@ url.place(x=150, y=450)
 
 
 down = tk.Button(window, text="Enter", fg="black" ,bg="yellow green" ,width=10 ,height=2, activebackground = "Red" ,font=('times', 15, ' bold '), command=prt)
-down.place(x=250, y=500)
+down.place(x=250, y=520)
 
 
 # message = tk.Label(window,  text = "", bg="slate blue",
@@ -116,7 +136,21 @@ down.place(x=250, y=500)
 # sep = ttk.Separator(window, orient='vertical')
 # sep.pack(padx=5, pady=5, fill= tk.Y, expand= True)
 
+# Making two extra slot for taking input of IP address and command in option SSH connection
+# Taking IP input
+# ip_label = tk.Label(window, text="Enter IP address:", fg="black", bg="light steel blue", font=('times', 15))
+# ip_label.place(x=11, y=490)
+# ip_entry = tk.Entry(window, width=40, bg="linen", fg="gray9",font=('times', 15, ' bold '))
+# ip_entry.place(x=150, y=490)
 
+# Taking Command input
+# cmd_label = tk.Label(window, text="Enter Command :", fg="black", bg="light steel blue", font=('times', 15))
+# cmd_label.place(x=11, y=530)
+# cmd_entry = tk.Entry(window, width=40, bg="linen", fg="gray9",font=('times', 15, ' bold '))
+# cmd_entry.place(x=150, y=530)
+
+# def ssh_func():
+#    return [ip_entry.get(),cmd_entry.get()]
 
 
 
